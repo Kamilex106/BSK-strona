1bc361d17c39c6bafd6f3134d7783957fe85be7b
Kamilex106 <115708858+Kamilex106@users.noreply.github.com>
2024-11-10 19:15:03 +0100

    Update templates.php

--- utils/templates.php
+++ utils/templates.php
@@ -21,6 +21,15 @@ function renderHeader($title = "Moderato - faster Allegro") {
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://www.google.com/recaptcha/api.js?render=$captchaKey"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-*********"></script>
+    <script>
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+
+    gtag('config', 'G-********');
+    </script>
     <script>
         if (navigator.webdriver && !window.location.href.includes('/bot-detected.php')) {
             window.location.href = "/bot-detected.php"
